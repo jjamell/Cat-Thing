13b8ad53475daee8f0f0a0f3d25169fefd734503
Jacob Jamell <jacob.jamell@gmail.com>
2015-08-18 21:39:49 +0000

    refactored Rectangle class

--- scripts/Rectangle.js
+++ scripts/Rectangle.js
@@ -1,33 +1,15 @@
-function Rectangle()
-{
-	this.left =		0;
-	this.top =		0;
-	this.width = 	0;
-	this.height = 	0;
-	this.center	=	{	x: 0 ,	
-						y: 0
-					};
-	
-	this.startupRectangle = function(left,top,width,height)
-	{
-		this.left	= 	left;
-		this.top	=	top;
-		this.right	=	left+width;
-		this.bottom	=	top+height;
-		this.center.x	=	left + width/2;
-		this.center.y	=	top + width/2;
-		return this;
-	}
-	
-	this.intersects = function(/*Rectangle*/ other)
-	{
-		if (this.right < other.left)	return false;
-		if (this.bottom < other.top)	return false;
-		if (this.left > other.right)	return false;
-		if (this.top > other.bottom)	return false;
-			
-		return true;
-		//return Math.atan2(this.center.y - other.center.y, this.center.x - other.center.x)/Math.PI;
-		
-	}
+function Rectangle(left, top, width, height) {
+	this.left = left || 0;
+	this.top = top || 0;
+	this.right = left + width || 0;
+	this.bottom = top + height || 0;
+	return this;
 }
+Rectangle.prototype.intersects = function ( /*Rectangle*/ other) {
+	if (this.right < other.left) return false;
+	if (this.bottom < other.top) return false;
+	if (this.left > other.right) return false;
+	if (this.top > other.bottom) return false;
+	return true;
+	//return Math.atan2(this.center.y - other.center.y, this.center.x - other.center.x)/Math.PI;
+};
